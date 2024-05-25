b5a8507fe83664142893869fe0d9059331a3d223
Sidhant Yadav <83953899+yadavsidhant@users.noreply.github.com>
2024-05-26 02:15:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,36 +3,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import json
-import pandas as pd
 from tensorflow.keras.preprocessing import image as keras_image
 from tensorflow.keras.models import load_model
 from tensorflow.keras.layers import DepthwiseConv2D
-import random
-
+import base64
+st.set_page_config(
+    page_title="Vision Track",
+    page_icon=":world_map:️",
+    layout="wide",
+)
 IMAGE_SIZE = 128
 
-# Define a custom DepthwiseConv2D layer
+# Read the image and encode it to base64
+def get_base64_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode()
+
+bg_image_base64 = get_base64_image("bg.png")
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewBlockContainer"] {{
+    background-image: url("data:image/png;base64,{bg_image_base64}");
+    background-size: cover;
+}}
+
+[data-testid="stHeader"] {{
+    background-color: rgba(0,0,0,0);
+}}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+element1 = """
+[data-testid="element-container"] {
+    background-color: rgba(0,0,0,0);
+}
+"""
+
 class CustomDepthwiseConv2D(DepthwiseConv2D):
     def __init__(self, *args, **kwargs):
         if 'groups' in kwargs:
             del kwargs['groups']
         super(CustomDepthwiseConv2D, self).__init__(*args, **kwargs)
 
-# Load the model
-try:
-    custom_objects = {
-        'DepthwiseConv2D': CustomDepthwiseConv2D
-    }
-    model = load_model('full_model.h5', custom_objects=custom_objects)
-except TypeError as e:
-    st.error(f"Error loading model: {e}")
+skin_cancer_model = load_model('./skin_cancer_classifier.h5')
+custom_objects = {'DepthwiseConv2D': CustomDepthwiseConv2D}
+full_model = load_model('full_model.h5', custom_objects=custom_objects)
 
-# Load class indices
 with open('class_indices.json', 'r') as json_file:
     class_indices = json.load(json_file)
 
 cancer_descriptions = {
-    'akiec': 'Actinic keratoses and intraepithelial carcinoma / Bowen\'s disease',
+    'akiec': "Actinic keratoses and intraepithelial carcinoma / Bowen's disease",
     'bcc': 'Basal cell carcinoma',
     'bkl': 'Benign keratosis-like lesions (solar lentigines / seborrheic keratoses and lichen-planus like keratoses)',
     'df': 'Dermatofibroma',
@@ -41,17 +64,6 @@ cancer_descriptions = {
     'vasc': 'Vascular lesions (angiomas, angiokeratomas, pyogenic granulomas, and hemorrhage)',
     'healthy': 'This is healthy skin'
 }
-
-# Load CSV files
-@st.cache_data()
-def load_csv_data():
-    test_ground_truth_file = 'ISIC2018_Task3_Test_GroundTruth.csv'
-    metadata_file = 'HAM10000_metadata.csv'
-    test_ground_truth_df = pd.read_csv(test_ground_truth_file)
-    metadata_df = pd.read_csv(metadata_file)
-    return test_ground_truth_df, metadata_df
-
-test_ground_truth_df, metadata_df = load_csv_data()
 
 def generate_grid_image(image_array, grid_size=30):
     grid = np.zeros((grid_size, grid_size))
@@ -97,41 +109,43 @@ def generate_confidence_grid(predictions, grid_size):
     return confidence_grid
 
 def predict_cancer(image_path):
-    img_id = image_path.split('/')[-1].split('.')[0]  # Extract image_id from file path
+    img = keras_image.load_img(image_path, target_size=(150, 150))
+    img_array = keras_image.img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
 
-    # Check if the image_id is in either of the CSV files
-    if img_id in test_ground_truth_df['image_id'].values:
-        row = test_ground_truth_df[test_ground_truth_df['image_id'] == img_id].iloc[0]
-        predicted_label = row['dx']
-        confidence = round(random.uniform(0.90, 0.99), 4)
-    elif img_id in metadata_df['image_id'].values:
-        row = metadata_df[metadata_df['image_id'] == img_id].iloc[0]
-        predicted_label = row['dx']
-        confidence = round(random.uniform(0.90, 0.99), 4)
+    prediction = skin_cancer_model.predict(img_array)
+    if prediction[0] > 0.5:
+        cancer_detected = True
+        cancer_label = 'Skin Cancer'
     else:
-        img = keras_image.load_img(image_path, target_size=(IMAGE_SIZE, IMAGE_SIZE))
-        img_array = keras_image.img_to_array(img) / 255.0
-        img_array = np.expand_dims(img_array, axis=0)
+        cancer_detected = False
+        cancer_label = 'Healthy Skin'
 
-        predictions = model.predict(img_array)
-        predicted_index = np.argmax(predictions, axis=1)[0]
-        predicted_label = list(class_indices.keys())[predicted_index]
-        confidence = predictions[0][predicted_index]
+    if not cancer_detected:
+        return cancer_label, None, None, None, None, None, None
+
+    img_full = keras_image.load_img(image_path, target_size=(IMAGE_SIZE, IMAGE_SIZE))
+    img_array_full = keras_image.img_to_array(img_full) / 255.0
+    img_array_full = np.expand_dims(img_array_full, axis=0)
+
+    predictions = full_model.predict(img_array_full)
+    predicted_index = np.argmax(predictions, axis=1)[0]
+    predicted_label = list(class_indices.keys())[predicted_index]
+    confidence = predictions[0][predicted_index]
 
     if predicted_label == 'healthy':
-        return predicted_label, confidence, None, None, None, None, img_array
+        return predicted_label, confidence, None, None, None, None, img_array_full
 
-    img = cv2.imread(image_path)
-    img = cv2.resize(img, (IMAGE_SIZE, IMAGE_SIZE))
+    img_cv2 = cv2.imread(image_path)
+    img_cv2 = cv2.resize(img_cv2, (IMAGE_SIZE, IMAGE_SIZE))
 
-    grid_image = generate_grid_image(img_array[0])
-    numerical_grid_image = generate_numerical_grid_image(img_array[0])
-    bounding_box_image = generate_bounding_box(img)
+    grid_image = generate_grid_image(img_array_full[0])
+    numerical_grid_image = generate_numerical_grid_image(img_array_full[0])
+    bounding_box_image = generate_bounding_box(img_cv2)
     confidence_grid = generate_confidence_grid(predictions, grid_size=numerical_grid_image.shape[0])
 
-    return predicted_label, confidence, grid_image, numerical_grid_image, bounding_box_image, confidence_grid, img_array
+    return predicted_label, confidence, grid_image, numerical_grid_image, bounding_box_image, confidence_grid, img_array_full
 
-# Streamlit app
 st.title("Skin Cancer Detection")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
@@ -145,26 +159,22 @@ if uploaded_file is not None:
 
     predicted_label, confidence, grid_image, numerical_grid_image, bounding_box_image, confidence_grid, original_image_array = predict_cancer('temp_image.jpg')
 
-    st.write(f"Predicted cancer type: {predicted_label}")
-    st.write(f"Confidence: {confidence:.2f}")
-    if original_image_array is not None:
-        st.write(f"Description: {cancer_descriptions.get(predicted_label, 'Unknown')}")
+    st.write(f"Predicted result: {predicted_label}")
+    if predicted_label != 'Healthy Skin':
+        st.write(f"Confidence: {confidence:.2f}")
+        st.write(f"Description: {cancer_descriptions[predicted_label]}")
 
         fig, axs = plt.subplots(1, 4, figsize=(20, 20))
 
-        # Display the grid image with contrasty markings
         axs[0].imshow(grid_image, cmap='hot', interpolation='nearest')
         axs[0].set_title('30x30 Grid with Markings')
 
-        # Display the numerical grid image with confusion matrix style
         axs[1].imshow(numerical_grid_image, cmap='binary', interpolation='nearest')
         axs[1].set_title('30x30 Numerical Grid')
 
-        # Display the bounding box on the image
         axs[2].imshow(bounding_box_image)
         axs[2].set_title('Bounding Box Image')
 
-        # Display the confidence grid
         axs[3].imshow(confidence_grid, cmap='hot', interpolation='nearest')
         axs[3].set_title('Confidence Grid')
 
